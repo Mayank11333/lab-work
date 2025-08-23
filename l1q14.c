c77a1564639527a923a4a3a5350d2d250ca8e1e1
Mayank11333 <mayank00939@gmail.com>
2025-08-23 20:47:17 +0530

    Add files via upload

---
+++ l1q14.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+  int f,c ;
+  printf("enter the number");
+  scanf("%d",&c);
+  f=((9c)/5)+32;
+  printf("the vlaue of f=%d",f);
+}
---
+++ l1q5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c,d,e,f;
+  printf("enter the number");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  d=a-b;
+  e=a*b;
+  f=a/b;
+  printf("the vlaue of c=%d",c);
+  printf("the vlaue of d=%d",d);
+  printf("the vlaue of e=%d",e);
+  printf("the vlaue of f=%d",f);
+}
